17bb2f0886bd21e78e3648dc60a22ef813adf407
DianaWangui <dianawanguikt048@gmail.com>
2024-04-14 20:17:48 +0300

    update typo

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def PerimeterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -26,4 +26,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
